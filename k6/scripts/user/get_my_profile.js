d0d5f29743dd96e6269f99d06d230e2e6dd192af
FlazeFy2 <flazen.edu@gmail.com>
2026-03-10 14:49:34 +0700

    test(performance) : add scenario multiple users see their profile simultaneously

---
+++ k6/scripts/user/get_my_profile.js
@@ -0,0 +1,52 @@
+import http from "k6/http"
+import { check, sleep } from "k6"
+import { loginHelper } from "../../helpers/auth.js"
+
+// Performance Test : PERF-USER-GMPR-001
+// Multiple users see their profile simultaneously
+
+// Env
+const BASE_URL = __ENV.BASE_URL
+
+// Test Config Data
+const MAX_P95_RESPONSE_TIME = "600"
+const MAX_FAILURE_RATE = "0.01"
+
+export const options = {
+    stages: [
+        { duration: "30s", target: 2 },  // warm up
+        { duration: "60s", target: 4 },  // 75% server's capacity test
+        { duration: "60s", target: 4 },  // sustain load
+        { duration: "30s", target: 0 },  // ramp down
+    ],
+    thresholds: {
+        http_req_duration: [`p(95)<${MAX_P95_RESPONSE_TIME}`],
+        http_req_failed: [`rate<${MAX_FAILURE_RATE}`],
+    },
+}
+
+export default function () {
+    // Get token from login
+    const token = loginHelper()
+
+    // Prepare the endpoint
+    const url = `${BASE_URL}/api/v1/user/my_profile`
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+    }
+
+    // Exec the endpoint
+    const res = http.get(url, params)
+
+    // Check response
+    check(res, {
+        "status is 200": (r) => r.status === 200,
+        "get my profile success": (r) => r.json("status") === "success",
+    })
+
+    // Pause before next iteration
+    sleep(1)
+}
