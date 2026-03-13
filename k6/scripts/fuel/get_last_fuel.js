2f32baa62a2d6d0b969c20977a4ccc670d42d367
FlazeFy2 <flazen.edu@gmail.com>
2026-03-13 14:42:25 +0700

    test(performance) : add scenario multiple users see their last fuel simultaneously

---
+++ k6/scripts/fuel/get_last_fuel.js
@@ -0,0 +1,52 @@
+import http from "k6/http"
+import { check, sleep } from "k6"
+import { loginHelper } from "../../helpers/auth.js"
+
+// Performance Test : PERF-FUEL-GLFL-001
+// Multiple users see their last fuel simultaneously
+
+// Env
+const BASE_URL = __ENV.BASE_URL
+
+// Test Config Data
+const MAX_P95_RESPONSE_TIME = "600"
+const MAX_FAILURE_RATE = "0.01"
+
+export const options = {
+    stages: [
+        { duration: "30s", target: 2 },  // warm up
+        { duration: "60s", target: 6 },  // 75% server's capacity test
+        { duration: "60s", target: 6 },  // sustain load
+        { duration: "30s", target: 0 },  // ramp down
+    ],
+    thresholds: {
+        http_req_duration: [`p(95)<${MAX_P95_RESPONSE_TIME}`],
+        http_req_failed: [`rate<${MAX_FAILURE_RATE}`],
+    },
+}
+
+export default function () {
+    // Get token from login
+    const token = loginHelper()
+
+    // Prepare the endpoint
+    const url = `${BASE_URL}/api/v1/fuel/last`
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+    }
+
+    // Exec the endpoint
+    const res = http.get(url, params)
+
+    // Check response
+    check(res, {
+        "status is 200": (r) => r.status === 200,
+        "get all fuel success": (r) => r.json("status") === "success",
+    })
+
+    // Pause before next iteration
+    sleep(1)
+}
