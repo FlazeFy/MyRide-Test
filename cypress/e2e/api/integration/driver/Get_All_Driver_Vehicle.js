6796656e1ce7cbcf04084bb22c2b9f09d469aad8
FlazeFy2 <flazen.edu@gmail.com>
2026-04-14 22:30:03 +0700

    test(integration) : add integration test for API GET all driver vehicle

---
+++ cypress/e2e/api/integration/driver/Get_All_Driver_Vehicle.js
@@ -0,0 +1,148 @@
+import '../../../../support/template'
+
+describe('MyRide Integration Test - Driver - Get : All Driver Vehicle', () => {
+    const method = 'get'
+    const url = '/api/v1/driver/vehicle'
+
+    const validateValidResponse = (dt) => {
+        cy.templateGet(200,dt, null)
+        expect(dt.body.message).contain('driver fetched')
+        
+        // Get Item Holder
+        const res = dt.body
+        expect(res).to.have.property('data')
+        const data = res.data
+        expect(data).to.be.an('object')
+
+        // Get List Key / Column
+        const stringFields = ['username','fullname','email','phone']
+        const stringNullableFields = ['telegram_user_id','vehicle_list']
+        const intBoolFields = ['telegram_is_valid']
+
+        // Validate Column
+        cy.templateValidateColumn(data.data, stringFields, 'string', false)
+        cy.templateValidateColumn(data.data, stringNullableFields, 'string', true)
+        cy.templateValidateColumn(data.data, intBoolFields, 'bool_number', false)
+    }
+
+    it('TC-INT-DR-009 : User Can See All Driver Vehicle With Valid Data', () => {
+        const payload = {
+            username : "flazefy",
+            password: 'nopass123',
+        }
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).as('UserCanSeeAllDriverVehicleWithValidData')
+            cy.get('@UserCanSeeAllDriverVehicleWithValidData').then(dt => {
+                validateValidResponse(dt)
+
+                // Check if all page accessible
+                const last_page = dt.body.data.last_page
+                cy.templatePagination(url, last_page, token)
+            })
+        })
+    })
+
+    it('TC-INT-DR-010 : User Can See All Driver Vehicle With Custom Item Per Page', () => {
+        const payload = {
+            username : "flazefy",
+            password: 'nopass123',
+        }
+        const itemPerPage = 1
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url: `${url}?per_page_key=${itemPerPage}`,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            }).as('UserCanSeeAllDriverVehicleWithCustomItemPerPage')
+            cy.get('@UserCanSeeAllDriverVehicleWithCustomItemPerPage').then(dt => {
+                validateValidResponse(dt)
+
+                // Check if item per page query same with data.length
+                const data = dt.body.data.data
+                cy.expect(data.length).to.equal(itemPerPage)
+            })
+        })
+    })
+
+    it('TC-INT-DR-011 : User Cant See All Driver Vehicle With Custom Invalid Item Per Page', () => {
+        const payload = {
+            username : "flazefy",
+            password: 'nopass123',
+        }
+        const itemPerPage = 'test'
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url: `${url}?per_page_key=${itemPerPage}`,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                failOnStatusCode: false,
+            }).as('UserCantSeeAllDriverVehicleWithCustomInvalidItemPerPage')
+            cy.get('@UserCantSeeAllDriverVehicleWithCustomInvalidItemPerPage').then(dt => {
+                cy.templateGet(400,dt, null)
+                expect(dt.body.message).contain('per_page_key is not a valid page')
+                
+                // Get Item Holder
+                const res = dt.body
+                expect(res).to.not.have.property('data')
+            })
+        })
+    })
+
+    it('TC-INT-DR-012 : User Cant See All Driver Vehicle With Empty Data', () => {
+        const payload = {
+            username : "testerempty",
+            password: 'nopass123',
+        }
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                failOnStatusCode: false,
+            }).as('UserCantSeeAllDriverVehicleWithEmptyData')
+            cy.get('@UserCantSeeAllDriverVehicleWithEmptyData').then(dt => {
+                cy.templateGet(404,dt, null)
+                expect(dt.body.message).contain('driver not found')
+                
+                // Get Item Holder
+                const res = dt.body
+                expect(res).to.not.have.property('data')
+            })
+        })
+    })
+
+    it('TC-INT-DR-013 : User Cant See All Driver Vehicle With Invalid Auth', () => {
+        cy.request({
+            method: method,
+            url,
+            headers: {
+                Accept: `application/json`
+            },
+            failOnStatusCode: false,
+        }).as('UserCantSeeAllDriverVehicleWithInvalidAuth')
+        cy.get('@UserCantSeeAllDriverVehicleWithInvalidAuth').then(dt => {
+            cy.templateGet(401,dt, null)
+            expect(dt.body.message).contain('you need to include the authorization token from login')
+            
+            // Get Item Holder
+            const res = dt.body
+            expect(res).to.not.have.property('data')
+        })
+    })
+})
