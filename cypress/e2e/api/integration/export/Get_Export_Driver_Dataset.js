f505a8b6e7b0664ab1df26b104b0b219d330a388
FlazeFy2 <flazen.edu@gmail.com>
2026-04-18 17:47:05 +0700

    test(integration) : add integration test for API GET export driver dataset (xlsx)

---
+++ cypress/e2e/api/integration/export/Get_Export_Driver_Dataset.js
@@ -0,0 +1,71 @@
+import '../../../../support/template'
+
+describe('MyRide Integration Test - Driver - Get : Export Driver Dataset', () => {
+    const method = 'get'
+    const url = '/api/v1/export/driver'
+
+    it('TC-INT-EX-016 : User Can Export Driver Dataset', () => {
+        const payload = {
+            username : "flazefy",
+            password: 'nopass123',
+        }
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).as('UserCanExportDriverDataset')
+            cy.get('@UserCanExportDriverDataset').then(dt => {
+                cy.templateGetExportExcel(dt)
+            })
+        })
+    })
+
+    it('TC-INT-EX-017 : User Cant Export Driver Dataset With Invalid Auth', () => {
+        cy.request({
+            method: method,
+            url,
+            headers: {
+                Accept: `application/json`
+            },
+            failOnStatusCode: false,
+        }).as('UserCantExportDriverDatasetWithInvalidAuth')
+        cy.get('@UserCantExportDriverDatasetWithInvalidAuth').then(dt => {
+            cy.templateGet(401,dt, null)
+            expect(dt.body.message).contain('you need to include the authorization token from login')
+            
+            // Get Item Holder
+            const res = dt.body
+            expect(res).to.not.have.property('data')
+        })
+    })
+
+    it('TC-INT-EX-018 : User Cant Export Driver Dataset With Empty Data', () => {
+        const payload = {
+            username : "testerempty",
+            password: 'nopass123',
+        }
+
+        cy.templateIntegrationLoginAPI(payload.username, payload.password).then(token => {
+            cy.request({
+                method: method,
+                url,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                failOnStatusCode: false,
+            }).as('UserCantExportDriverDatasetWithEmptyData')
+            cy.get('@UserCantExportDriverDatasetWithEmptyData').then(dt => {
+                cy.templateGet(404,dt, null)
+                expect(dt.body.message).contain('driver not found')
+                
+                // Get Item Holder
+                const res = dt.body
+                expect(res).to.not.have.property('data')
+            })
+        })
+    })
+})
